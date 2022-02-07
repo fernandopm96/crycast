077f5123d1e9c932d987d4aebf37bfb2cbc530db
fernando <fernandopanos.alu@iespacomolla.es>
2022-02-07 14:25:38 +0100

    icono sol

--- app/src/main/java/com/example/crycast/ui/view/SelectUsers.kt
+++ app/src/main/java/com/example/crycast/ui/view/SelectUsers.kt
@@ -1,9 +1,12 @@
 package com.example.crycast.ui.view
 
+import android.annotation.SuppressLint
 import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
@@ -16,20 +19,25 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.crycast.R
 import com.example.crycast.model.User
+import com.example.crycast.ui.theme.PrimaryDark
 import com.example.crycast.ui.theme.PrimaryLight
+import com.example.crycast.viewmodel.DataStoreViewModel
 import com.example.crycast.viewmodel.MainViewModel
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
 @Composable
 fun selectUsers(){
     var mainViewModel: MainViewModel = viewModel()
     var scope = rememberCoroutineScope()
+    var context = LocalContext.current
 
     Scaffold(
         topBar = { topBarSelectUsers() },
@@ -42,7 +50,13 @@ fun selectUsers(){
                     var groupId = mainViewModel.createGroup(newGroup)
                     mainViewModel.setUsersToGroup(groupId, selectedUsers)
                     selectedUsers = mutableListOf()
+                    Toast.makeText(
+                        context,
+                        "Participantes añadidos",
+                        Toast.LENGTH_LONG
+                    ).show()
                 }
+                navHostController.popBackStack()
             }
         }) {
             Icon(imageVector = Icons.Filled.Done, contentDescription = "Aceptar")
@@ -88,6 +102,7 @@ fun usersToAdd(){
 
 var selectedUsers: MutableList<User> = mutableListOf()
 
+@SuppressLint("CoroutineCreationDuringComposition", "UnrememberedMutableState")
 @Composable
 fun userItem(user: User){
 
@@ -108,8 +123,9 @@ fun userItem(user: User){
                 }
             }
         }
-        .background(if (selected.value) PrimaryLight else Color.White)
+        .background(if (selected.value) PrimaryLight else MaterialTheme.colors.onPrimary)
     ) {
+
         Column(
             verticalArrangement = Arrangement.Center,
             modifier = Modifier
@@ -133,7 +149,6 @@ fun userItem(user: User){
                 .fillMaxHeight()
                 .weight(0.8f)) {
             Text(user.name,
-                color = if(selected.value) Color.White else Color.Black,
                 fontSize = (16.sp),
                 modifier = Modifier.absolutePadding(10.dp,15.dp,0.dp, 0.dp))
         }
